691cc103948860f2f0b95a797f0326a667f42f90
carlosnuno1 <carnuno@ucsc.edu>
2024-06-13 01:45:20 -0700

    time change

--- src/Scenes/end.js
+++ src/Scenes/end.js
@@ -16,7 +16,7 @@ class End extends Phaser.Scene {
 
     update() {
         //text
-        this.add.text(200, 200, '                             You Did It!!\n \n                                  Nice!!\n                      Press R to play again\n\n\n\n                             Dev Times: \n                             Level1 3.6\n                             Level2 5.7\n                             Level3 3.6\n                             Level4 7.6', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', fontSize: '50px'});
+        this.add.text(200, 200, '                             You Did It!!\n \n                                  Nice!!\n                      Press R to play again\n\n\n\n                             Dev Times: \n                             Level1 3.6\n                             Level2 4.7\n                             Level3 3.6\n                             Level4 7.6', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', fontSize: '50px'});
         //event handling
         if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
             this.scene.start("level2Scene");
